d3f314cf86c971204e6441049f54717e8d45bd78
Глеб <krukovskiygleb@gmail.com>
2024-12-18 01:15:59 +0300

    new era!

--- src/main/java/com/example/ULC/controllers/ExcelController.java
+++ src/main/java/com/example/ULC/controllers/ExcelController.java
@@ -4,6 +4,7 @@ import com.example.ULC.services.DownloadService;
 import com.example.ULC.services.ExcelComparatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,8 +13,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-@RestController
-@RequestMapping("/api/excel")
+@Controller
 public class ExcelController {
     static final String idYT = "1rZOYBNWty6BTm5OFDuaRsWmdTW7QFjKHIyZpTuqx4nE";
     static final String id1C = "1Qbh5rSJ9Xbq6-Da2gC4qQNTBgh3j97h7zracHrIgxSA";
@@ -22,15 +22,20 @@ public class ExcelController {
     @Autowired
     private DownloadService downloadService;
 
-    @PostMapping("/compare")
-    public ResponseEntity<Map<String, List<List<String>>>> compareExcelFiles(@RequestParam("file1") MultipartFile file1,
-                                                                       @RequestParam("file2") MultipartFile file2) {
-        try {
-            Map<String, List<List<String>>> matches = excelComparatorService.compareExcelFiles(file1, file2);
-            return ResponseEntity.ok(matches);
-        } catch (IOException e) {
-            return ResponseEntity.badRequest().body(null);
-        }
+//    @PostMapping("/compare")
+//    public ResponseEntity<Map<String, List<List<String>>>> compareExcelFiles(@RequestParam("file1") MultipartFile file1,
+//                                                                       @RequestParam("file2") MultipartFile file2) {
+//        try {
+//            Map<String, List<List<String>>> matches = excelComparatorService.compareExcelFiles(file1, file2);
+//            return ResponseEntity.ok(matches);
+//        } catch (IOException e) {
+//            return ResponseEntity.badRequest().body(null);
+//        }
+//    }
+    @GetMapping("/")
+    public String mainPage()
+    {
+        return "main_page";
     }
 
     @GetMapping("/groups")
@@ -38,7 +43,7 @@ public class ExcelController {
        File fileYT = downloadService.downloadExcel(idYT);
        File file1C = downloadService.downloadExcel(id1C);
        excelComparatorService.compareExcelFiles(file1C, fileYT);
-
+       return "groups";
     }
 
 }
